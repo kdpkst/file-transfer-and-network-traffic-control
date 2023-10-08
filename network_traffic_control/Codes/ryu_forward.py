7f8d259742e772afa9dc0d2043060c790ead541b
kdpkst <jammiechen0@gmail.com>
2023-10-08 22:07:46 +0800

    add copyright info for ryu-based app

--- network_traffic_control/Codes/ryu_forward.py
+++ network_traffic_control/Codes/ryu_forward.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
@@ -18,8 +33,8 @@ class SimpleSwitch13(app_manager.RyuApp):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures,
-                CONFIG_DISPATCHER)  # the decorator tells Ryu when the decorated function should be called
+    # the decorator tells Ryu when the decorated function should be called
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)  
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
